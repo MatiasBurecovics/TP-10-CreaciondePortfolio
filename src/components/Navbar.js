b9da1c047d2c3a8fddcfa6bbcc73ffcf9b855ffa
MatiasBurecovics <111394291+MatiasBurecovics@users.noreply.github.com>
2023-10-23 14:15:57 -0300

    Add files via upload

---
+++ src/components/Navbar.js
@@ -0,0 +1,23 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Link } from 'react-router-dom';
+
+const CustomNavbar = () => {
+
+    return (
+      <Navbar bg="white" variant="white">
+      <Container>
+          <Nav className="ms-auto" >
+              <Link className="nav-link active" to="">Home</Link>
+              <Link className="nav-link active" to="/infopersonal">Info Personal</Link>
+              <Link className="nav-link active" to="/miscreaciones">Mis creaciones</Link>
+              <Link className="nav-link active" to="/favoritos">Favoritos </Link>
+          </Nav>
+      </Container>
+  </Navbar>
+    );
+}
+
+export default CustomNavbar;
---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from './components/Layout';
+import Home from '../src/screens/Home';
+import MisCreaciones from '../src/screens/MisCreaciones'
+import InfoPersonal from '../src/screens/InfoPersonal';
+import Favoritos from '../src/screens/Favoritos'
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/miscreaciones" element={<MisCreaciones />}></Route>
+          <Route path="/infopersonal" element={<InfoPersonal />}></Route>
+          <Route path="/favoritos" element={<Favoritos />}></Route>
+          </Route>
+        </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
